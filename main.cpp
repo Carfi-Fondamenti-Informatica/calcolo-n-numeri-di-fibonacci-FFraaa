5278af1b22381d8cccefe50bf9115d5c5fbf6ff8
FFraaa <91596644+FFraaa@users.noreply.github.com>
2021-11-13 21:08:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int a=0,b=1,n=0,c=a+b;
+cin>>n;
+cout<<b<<endl;
+for(int i=2;i<(n+1);i++){
+    if(c<=n){
+        cout<<c<<endl;
+        a=b;
+        b=c;
+        c=a+b;
+    }else{
+        cout<<"errore"<<endl;
+    }
 }
+    return 0;
+}
+
